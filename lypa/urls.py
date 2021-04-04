b7179e0b6685c285ff1c8c95ae89c5e0bf24d8db
fosofou <57597439+fosofou@users.noreply.github.com>
2021-04-04 19:56:58 +0500

    aga

--- lypa/urls.py
+++ lypa/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('django.contrib.auth.urls')),
     path('',include('blog.urls'))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
