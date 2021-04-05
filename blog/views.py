c28c37696b4b41c7dbfd5e6996a6c48fe1e64589
fosofou <57597439+fosofou@users.noreply.github.com>
2021-04-05 14:51:00 +0500

    vrode rabotaet

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
-
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -9,3 +9,7 @@ def post_list(request):
 
 def graph(request):
     return render(request, 'blog/test.html',{})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {"post":post})
